730098daf97601e3fafc3f9962c4b65c1dff4e30
Elek, Márton <elek@users.noreply.github.com>
2016-12-20 12:42:03 +0100

    fix default consul location with adding the port

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORKDIR=$DIR/c
-CONSUL_SERVER=${CONSUL_SERVER:-localhost}
+CONSUL_SERVER=${CONSUL_SERVER:-localhost:8500}
 export CONSUL_COMPOSE_PATH=${CONSUL_COMPOSE_PATH:-/compose/docker-compose.yaml}
 mkdir -p $WORKDIR
 consul-template -consul $CONSUL_SERVER -template=<(echo '{{key (env "CONSUL_COMPOSE_PATH")}}'):$WORKDIR/docker-compose.yaml -exec "$DIR/composer.sh $WORKDIR/docker-compose.yaml"
